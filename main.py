7edfa5217e5a871451c05b13cefb43850a16011e
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-03 21:49:11 +0000

    Alternative implementation of error handling

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ fruits = ["Apple", "Pear", "Orange"]
 
 # TODO: Catch the exception and make sure the code runs without crashing.
 def make_pie(index):
-    fruit = fruits[index]
-    print(fruit + " pie")
+    try:
+        fruit = fruits[index]
+    except IndexError:
+        print("Fruit pie")
+    else:
+        print(fruit + " pie")
 
 
-try:
-    make_pie(2)
-except IndexError as error_message:
-    print(f"The code fail because of: '{error_message}'")
-else:
-    print("Execution completed successfully")
+# try:
+make_pie(4)
+# except IndexError as error_message:
+#     print(f"The code fail because of: '{error_message}'")
+# else:
+#     print("Execution completed successfully")
